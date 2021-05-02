464d9e663889701eff41b20028eb95d6cdaf97f9
baolongt <tranbaolong14@gmail.com>
2021-05-02 19:37:55 +0700

    svg + animation added

--- src/components/landing components/SearchBar.jsx
+++ src/components/landing components/SearchBar.jsx
@@ -1,29 +1,34 @@
 import React from "react";
-
+import searchIcon from "../resource/svg/search.svg";
+import "./Button.css";
 const SearchBar = () => {
   return (
     <div
-      className="grid-cols-9 grid text-center mt-6 h-10
-    lg:h-16
-    xl:h-20
+      className="p-8
     "
     >
-      <input
-        className="col-start-3 col-end-7 rounded-l text-xs text-black
-        lg:text-base
-        xl:text-xl
-        "
-        type="text"
-        placeholder="Find game"
-      />
-      <button
-        className="col-start-7 col-end-8 bg-yellow-400 rounded-r text-xs
-        lg:text-base
-        xl:text-xl
-        "
+      <div
+        className="bg-white flex items-center rounded-full shadow-xl
+        lg:w-11/12 lg:ml-12
+        xl:w-7/12 xl:ml-72
+      "
       >
-        Search
-      </button>
+        <input
+          className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none
+          xl:text-xl
+          "
+          type="text"
+          placeholder="Find game"
+        />
+        <div className="p-4">
+          <button
+            className="bg-yellow-400 text-white rounded-full p-2
+           hover:bg-yellow-300 focus:outline-none w-12 h-12 flex items-center justify-center"
+          >
+            <img src={searchIcon} className="" alt="svg" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
