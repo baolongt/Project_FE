a1ce6ad05f66601ee47f64c71b521b4fb0848341
baolongt <tranbaolong14@gmail.com>
2021-05-04 12:07:12 +0700

    fetch api issue

--- src/API/DataProvider.jsx
+++ src/API/DataProvider.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import API from "./HTTP";
 
-const data = API.get("/app-ids/578080").then((res) => {
-  return res.data;
-});
 const DataContext = React.createContext();
 
+// hooks
+export function useDataContext() {
+  return useContext(DataContext);
+}
+
 export default function DataProvider({ children }) {
-  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      API.get("/app-ids/578080").then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        console.log(res.data);
+        setIsLoading(false);
+      });
+    }
+    fetchData();
+  }, [data]);
+  return (
+    <DataContext.Provider value={{ data, isLoading }}>
+      {children}
+    </DataContext.Provider>
+  );
 }
