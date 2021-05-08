4fed09af717f4d7691ebce1afc24d73e45025f27
unknown <daohuuduc.910@gmail.com>
2021-05-08 21:45:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import "./App.css";
 import SearchComponent from "./components/landing components/SearchComponent/SearchComponent";
 import { Route, BrowserRouter } from "react-router-dom";
 import Game from "./components/GameDetail/Game";
+import TopGames from "./components/TopGame/TopGames";
+import DataProvider from "./API/DataProvider";
 
 function App() {
   return (
-      <DataProvider>
-        <BrowserRouter>
-          <Route path="/" exact component={SearchComponent} />
-          <Route path="/top" exact component={TopGames} />
-          <Route path="/info/:id" children={<Game />} />
-        </BrowserRouter>
-      </DataProvider>
-    </>
+    <DataProvider>
+      <BrowserRouter>
+        <Route path="/" exact component={SearchComponent} />
+        <Route path="/top" exact component={TopGames} />
+        <Route path="/info/:id" children={<Game />} />
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
